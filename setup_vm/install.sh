321f98c61b228af70467d991879cd020d370b7fc
hugokovac <kovmaxime@gmail.com>
2024-04-13 15:30:54 +0200

    new struct

--- setup_vm/install.sh
+++ setup_vm/install.sh
@@ -2,3 +2,7 @@
 
 bash ./install_gns3.sh
 bash ./install_docker.sh
+
+sudo chgrp $USER /usr/bin/dumpcap
+sudo chmod 754 /usr/bin/dumpcap
+sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' /usr/bin/dumpcap
